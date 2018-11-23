ab2f469c845662c2b638879cca732cc11f139bf3
Sano Suguru <sa.50.00.no.riku+github@gmail.com>
2018-11-23 17:50:57 +0900

    show events

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,5 +1,6 @@
 import React, { Component} from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 
 import { readEvents } from '../actions';
 
@@ -7,16 +8,36 @@ class EventsIndex extends Component {
   componentDidMount() {
     this.props.readEvents();
   }
+  renderEvents() {
+    const { events } = this.props;
+    return _.map(events, event => (
+      <tr key={event.id}>
+        <td>{event.id}</td>
+        <td>{event.title}</td>
+        <td>{event.body}</td>
+      </tr>
+    ));
+  }
+
   render() {
     return (
-      <h1>
-        Hello, React!
-      </h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.renderEvents()}
+        </tbody>
+      </table>
     );
   }
 }
 
 export default connect(
-  state => ({}),
+  state => ({ events: state.events }),
   { readEvents }
 )(EventsIndex);
